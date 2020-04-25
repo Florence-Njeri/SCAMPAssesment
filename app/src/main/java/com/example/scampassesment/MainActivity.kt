b08c9a39232e181433b12af3ad757319fab9a47b
Florence-Njeri <flonsie17@gmail.com>
2020-04-25 20:57:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/scampassesment/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.scampassesment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.scampassesment.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
