b0fb43b7c2b3d8faafa21aea393dd4d638aa40cd
Florence-Njeri <flonsie17@gmail.com>
2020-04-28 08:48:25 +0300

    add jetpack navigation

--- app/src/main/java/com/example/scampassesment/MainActivity.kt
+++ app/src/main/java/com/example/scampassesment/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.scampassesment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.scampassesment.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +17,13 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
         }
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
---
+++ app/src/main/java/com/example/scampassesment/model/CoronavirusCasesModel.kt
@@ -0,0 +1,11 @@
+package com.example.scampassesment.model
+
+data class WorldTotalCases(
+    val TotalConfirmed:Int,
+    val TotalDeaths:Int,
+    val TotalRecovered:Int
+)
+
+//data class CountryTotalCases(
+//
+//)
