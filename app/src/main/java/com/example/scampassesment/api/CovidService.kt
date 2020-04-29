a54704c0ad379816bb0a423f7dcc46ad6a3e26bd
Florence-Njeri <flonsie17@gmail.com>
2020-04-29 09:43:42 +0300

    generate new data class for the Statistics Summary WorldWide

--- app/src/main/java/com/example/scampassesment/api/CovidService.kt
+++ app/src/main/java/com/example/scampassesment/api/CovidService.kt
@@ -1,6 +1,7 @@
 package com.example.scampassesment.api
 
 import com.example.scampassesment.model.Countries
+import com.example.scampassesment.model.Summary
 import com.example.scampassesment.model.WorldTotalCases
 import retrofit2.Call
 import retrofit2.http.GET
@@ -18,5 +19,9 @@ interface CovidService {
     //Get world statistics
     @GET("/world/total")
     fun getWorldStatistics(): Call<WorldTotalCases>
+
+    //Get world statistics
+    @GET("/summary")
+    fun getWorldSummary(): Call<List<Summary>>
 
 }
--- app/src/main/java/com/example/scampassesment/model/CoronavirusCasesModel.kt
+++ app/src/main/java/com/example/scampassesment/model/CoronavirusCasesModel.kt
@@ -22,6 +22,41 @@ data class Countries(
     val Country:String
 )
 
+/**
+ * https://api.covid19api.com/summary
+ */
+data class Summary(
+    val Countries: List<Country>,
+    val Date: String,
+    val Global: Global
+)
+/**
+ * Statistics per country
+ * Get Country name and TotalConfirmed
+ */
+
+data class Country(
+    val Country: String,
+    val CountryCode: String,
+    val Date: String,
+    val NewConfirmed: Int,
+    val NewDeaths: Int,
+    val NewRecovered: Int,
+    val Slug: String,
+    val TotalConfirmed: Int,
+    val TotalDeaths: Int,
+    val TotalRecovered: Int
+)
+//Global statistics
+data class Global(
+    val NewConfirmed: Int,
+    val NewDeaths: Int,
+    val NewRecovered: Int,
+    val TotalConfirmed: Int,
+    val TotalDeaths: Int,
+    val TotalRecovered: Int
+)
+
 //Get a list of all countries
 
 //then get the statistics per country
