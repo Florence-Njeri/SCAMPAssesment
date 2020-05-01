e879c5a309e2ff32438d4fe54feb806fed63259f
Florence-Njeri <flonsie17@gmail.com>
2020-05-01 19:25:22 +0300

    add Country Statistics details and add it to navigation graph

---
+++ app/src/main/java/com/example/scampassesment/ui/main/CountryStatisticsDetails.kt
@@ -0,0 +1,32 @@
+package com.example.scampassesment.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.scampassesment.R
+
+class CountryStatisticsDetails : Fragment() {
+
+    companion object {
+        fun newInstance() = CountryStatisticsDetails()
+    }
+
+    private lateinit var viewModel: CountryStatisticsDetailsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.country_statistics_details_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(CountryStatisticsDetailsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/scampassesment/ui/main/CountryStatisticsDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.scampassesment.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class CountryStatisticsDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
